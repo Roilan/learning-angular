b00b034032b31595400016cd4726b58e33706fde
Roilan <roilan14@gmail.com>
2014-12-13 18:01:49 -0500

    and success

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
 (function(){
 	var app = angular.module('movie', []);
 
-	app.controller('MovieCtrl', function($scope) {
+	app.controller('MovieCtrl', function($scope, $http) {
+
+		$http.get('http://www.omdbapi.com/?t=iron man&y=&plot=short&r=json')
+			.success(function(data) {
+				$scope.data = data;
+
+				$scope.name = data.Title;
+				$scope.release = data.Released;
+				$scope.length = data.Runtime;
+				$
+			});
 
 	});
 })();
