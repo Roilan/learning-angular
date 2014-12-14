03a8e8f4665ffaf06b5f54054ea472021d15c6d9
Roilan <roilan14@gmail.com>
2014-12-13 22:50:45 -0500

    it works

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,21 +4,18 @@
 	app.controller('MovieCtrl', function($scope, $http) {
 		$scope.movieName;
 
-		$scope.update = function(searchName) {
-			$scope.movieName = angular.copy(searchName);
+		$scope.search = function(searchMovie) {
+			$scope.movieName = angular.copy(searchMovie);
+
+			$http.get('http://www.omdbapi.com/?t=' + $scope.movieName + '&y=&plot=short&r=json')
+				.success(function(data) {
+					$scope.name = data.Title;
+					$scope.release = data.Released;
+					$scope.length = data.Runtime;
+					$scope.description = data.Plot;
+					$scope.rating = data.imdbRating;
+				});
 		}
-
-		$http.get('http://www.omdbapi.com/?t=iron man&y=&plot=short&r=json')
-			.success(function(data) {
-				$scope.data = data;
-
-				$scope.name = data.Title;
-				$scope.release = data.Released;
-				$scope.length = data.Runtime;
-				$scope.description = data.Plot;
-				$scope.rating = data.imdbRating;
-
-			});
 
 	});
 })();
