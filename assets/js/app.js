ce936d7551ca92dc0b19c41bdb7546078316c2bf
Roilan <roilan14@gmail.com>
2014-12-13 20:35:28 -0500

    input text and btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@
 	var app = angular.module('movie', []);
 
 	app.controller('MovieCtrl', function($scope, $http) {
+		$scope.movieName;
+
+		$scope.update = function(searchName) {
+			$scope.movieName = angular.copy(searchName);
+		}
 
 		$http.get('http://www.omdbapi.com/?t=iron man&y=&plot=short&r=json')
 			.success(function(data) {
@@ -10,7 +15,9 @@
 				$scope.name = data.Title;
 				$scope.release = data.Released;
 				$scope.length = data.Runtime;
-				$
+				$scope.description = data.Plot;
+				$scope.rating = data.imdbRating;
+
 			});
 
 	});
